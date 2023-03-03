8277ab3ef32f4227cf0056fa23e68212f6746947
Monica Wheeler <monica@kajabi.com>
2023-03-03 09:42:26 -0600

    Remove loader from non-loading state

--- pages/index.js
+++ pages/index.js
@@ -74,25 +74,6 @@ export default function Home() {
             </svg>
             </span> : <>{result}</>}
         </div>
-
-
-        <span>
-            <svg className={styles.loader} width="140" height="140" viewBox="0 0 280 280" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <g>
-                <line x1="59.9833" y1="140.333" x2="219.978" y2="139" stroke="#000" stroke-width="4"/>
-                <circle cx="60" cy="140" r="5" fill="#000"/>
-                <circle cx="220" cy="139" r="5" fill="#000"/>
-              </g>
-              <path className={styles.loaderCircle} d="M109.957 122.655L140 105.309L170.043 122.655V157.345L140 174.691L109.957 157.345V122.655Z" stroke="#000" stroke-width="4"/>
-              <circle className={styles.loaderCircle} cx="140" cy="140" r="13" stroke="#f5d77b" stroke-width="4"/>
-              <circle className={styles.loaderCircle} cx="110" cy="192" r="13" stroke="#f7a78f" stroke-width="4"/>
-              <circle className={`${styles.loaderCircle} ${styles.loaderCircle_s}`} cx="85" cy="232" r="8" stroke="#82c7c5" stroke-width="4"/>
-              <circle className={styles.loaderCircle} cx="170" cy="88" r="13" stroke="#82c7c5" stroke-width="4"/>
-              <circle className={`${styles.loaderCircle} ${styles.loaderCircle_s}`} cx="110" cy="192" r="5" fill="#f7a78f"/>
-              <circle className={`${styles.loaderCircle} ${styles.loaderCircle_s}`} cx="185" cy="61" r="5" fill="#f5d77b"/>
-            </svg>
-            </span>
-
       </main>
     </div>
   );
